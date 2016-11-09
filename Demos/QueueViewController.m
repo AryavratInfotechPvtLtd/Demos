a9aa991df6fa6e12c0478d5fa16830246ec0f747
Rohit Parihar <user@users-MacBook-Pro.local>
2016-11-09 15:58:23 +0530

    09November2016(03:58)

--- Demos/QueueViewController.m
+++ Demos/QueueViewController.m
@@ -36,7 +36,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    imgurls = @[@"http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", @"http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", @"http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", @"http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"];
+    imgurls = @[@"http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", @"http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", @"http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", @"http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
     
     OperationQueue = [[NSOperationQueue alloc] init];
     // Do any additional setup after loading the view, typically from a nib.
