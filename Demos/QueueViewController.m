17b1540cdf1603828614333b170004b0f58959ca
Rohit Parihar <user@users-MacBook-Pro.local>
2016-11-11 15:15:51 +0530

    11November2016(03:15)

--- Demos/QueueViewController.m
+++ Demos/QueueViewController.m
@@ -29,12 +29,6 @@
     return Img;
 }
 
-+(UIImage *)DownloadImageFromUrlWith : (NSString *)urlImg
-{
-    UIImage *Img = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:urlImg]]];
-    return Img;
-}
-
 @end
 
 @interface QueueViewController ()
