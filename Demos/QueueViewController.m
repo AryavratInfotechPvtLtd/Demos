e94c37f8c564b32edcb978d7ff25ce0cb1ed4ac3
Rohit Parihar <user@users-MacBook-Pro.local>
2016-11-29 11:03:17 +0530

    UIViewController with UITitle

--- Demos/QueueViewController.m
+++ Demos/QueueViewController.m
@@ -29,7 +29,6 @@
         return Img;
 }
 
-    
 @end
 
 @interface QueueViewController ()
@@ -47,6 +46,7 @@
     imgurls = @[@"http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", @"http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", @"http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", @"http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"];
     
     OperationQueue = [[NSOperationQueue alloc] init];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
